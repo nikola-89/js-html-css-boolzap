023e0c6f7790a651664dcb37a842d659b6bc2950
Nicola <nicola.ba1989@gmail.com>
2020-01-27 03:13:11 +0100

    [JS] | riscritto tutto, array based

--- js/app.js
+++ js/app.js
@@ -1,25 +1,49 @@
 $(document).ready(function() {
     // ***************************
-    // ARRAY CONTATTI **** START
-    var contactsIdArray = [];
-    $('.contact').each(
-        function(index) {
-        contactsIdArray.push({
-            'id': index + '-' + getRandomIntInclusive(1000, 9999) + '-' + getRandomIntInclusive(10000, 99999),
-            'name': $(this).find('.contact-name h4').text(),
-            'contact': $(this).clone(),
-            'msg': $('.msg-conversation .message').clone(),
-        }
-    );
-        console.log(contactsIdArray);
-        }
-    );
-    // ARRAY CONTATTI **** END
-
-    // INPUT CERCA CONTATTI **** START
-    $(".finder-box .finder input").on("keyup", function() {
+    // ARRAY CONTATTI ****
+    var contactsIdArray = [
+        { name : 'Bill Gates',
+        id : 1000,
+        img : 'img/contact-1.jpg',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'Guido van Rossum',
+        id : 1001,
+        img : 'img/contact-2.jpg',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'George Francis Hotz',
+        id : 1002,
+        img : 'img/contact-3.jpg',
+        isSelected : false,
+        msg : []
+        },
+    ];
+    // ***************************
+    // CREAZIONE BOX CONTATTI da Array |||
+    for (var i = 0; i < contactsIdArray.length; i++) {
+        // *************************
+        var templateContact = $('.template .contact').clone();
+        // | img contatto
+        templateContact.find('.contact-avatar img').attr('src', contactsIdArray[i].img);
+        // | nome contatto
+        templateContact.find('.contact-name h4').text(contactsIdArray[i].name);
+        // | orario ultimo messaggio contatto
+        templateContact.find('.contact-name small').text('00:00');
+        // | ultimo messaggio contatto
+        templateContact.find('.contact-last-message p').text('TEXT');
+        // | assegnazione id contatto
+        templateContact.attr('contact-id', contactsIdArray[i].id);
+        // ---> append
+        $('.contacts-list').append(templateContact);
+    }
+    // ***************************
+    // INPUT CERCA CONTATTI |||
+    $(".finder input").on("keyup", function() {
         var searchStr = $(this).val().toLowerCase().trim();
-        $('.contact .contact-info .contact-name h4').each(
+        $('.contact-name h4').each(
             function() {
                 if ($(this).text().toLowerCase().indexOf(searchStr) == -1) {
                     $(this).parents('.contact').hide();
@@ -29,43 +53,41 @@ $(document).ready(function() {
             }
         );
     });
-    // INPUT CERCA CONTATTI **** END
-
-    // INVIO MESSAGGIO = CLICK - TASTO ENTER | ICON INPUT SWAP **** START
+    // ***************************
+    // INVIO MESSAGGIO = CLICK - TASTO ENTER | ICON INPUT SWAP |||
     $('.msg-input input').keyup(
         function() {
             resetIconSend();
         }
     );
-
+    // ***************************
     $('.msg-input input').keydown(
         function() {
             if (event.which == 13) {
                 resetIconSend();
-                if (sendMessage()) {
+                if (sendMessage(contactsIdArray)) {
                     setTimeout(
                         function() {
-                            MessageReceived();
+                            incomingMessage(contactsIdArray);
                         }, 1000);
                 }
             }
         }
     );
-
+    // ***************************
     $('.msgsend-ico').click(
         function() {
             resetIconSend();
-            if (sendMessage()) {
+            if (sendMessage(contactsIdArray)) {
                 setTimeout(
                     function() {
-                        MessageReceived();
+                        incomingMessage(contactsIdArray);
                     }, 1000);
             }
         }
     );
-    // INVIO MESSAGGIO = CLICK - TASTO ENTER | ICON INPUT SWAP **** END
-
-    // CHEVRON SHOW/HIDDEN IN BOX MESSAGGIO || BOX CONTATTO**** START
+    // ***************************
+    // CHEVRON SHOW/HIDDEN IN BOX MESSAGGIO || BOX CONTATTO |||
     $(document).on("mouseenter mouseleave", ".message", function(e) {
         if (e.type == "mouseenter") {
             $(this).find('.chevron').removeClass('hidden');
@@ -73,63 +95,129 @@ $(document).ready(function() {
             $(this).find('.chevron').addClass('hidden');
         }
     });
-
+    // ***************************
     $(document).on("mouseenter mouseleave click", ".contact", function(e) {
         if (e.type == "mouseenter") {
             $(this).find('.chevron').removeClass('hidden');
-            if (contactIsSelected(this) == false) {
-                $(this).css("background-color", "rgb(245, 245, 245)");
+            for (var i = 0; i < contactsIdArray.length; i++) {
+                if (!contactsIdArray[i].isSelected) {
+                    // | rimuove colore sfondo box tutti i contatti
+                    $('.contact').removeClass('contact-mouseenter');
+                    $('.contact').removeClass('contact-mouseleave');
+                    // | colore sfondo box contatto
+                    $(this).addClass('contact-mouseenter');
+                }
             }
         } else if (e.type == "mouseleave") {
             $(this).find('.chevron').addClass('hidden');
-            if (contactIsSelected(this) == false) {
-                $(this).css("background-color", "rgba(0, 0, 0, 0)");
+            for (var i = 0; i < contactsIdArray.length; i++) {
+                if (!contactsIdArray[i].isSelected) {
+                    // | rimuove colore sfondo box tutti i contatti
+                    $('.contact').removeClass('contact-mouseenter');
+                    $('.contact').removeClass('contact-mouseleave');
+                    // | colore sfondo box contatto
+                    $(this).addClass('contact-mouseleave');
+                }
             }
         } else {
-            $(".contact").css("background-color", "rgba(0, 0, 0, 0)");
-            $(this).css("background-color", "rgb(235, 235, 235)");
-            contactFocus(this);
-            $('.stay-connected-phone').addClass('hidden');
-            $('.message-box').removeClass('hidden');
+            for (var i = 0; i < contactsIdArray.length; i++) {
+                // | reset isSelected to false in array
+                $(contactsIdArray[i].isSelected = false);
+                // | rimuove colore sfondo box tutti i contatti
+                $('.contact').removeClass('contact-click');
+                if (contactIdFinder(this) == contactsIdArray[i].id) {
+                    // | schermata conversazione reset
+                    messageBoxReset();
+                    // | isSelected true in array
+                    $(contactsIdArray[i].isSelected = true);
+                    // | nome contatto tramite id - schermata conversazione
+                    $('.msg-info-contact-name h4').text(contactsIdArray[i].name);
+                    // | img contatto tramite id - schermata conversazione
+                    $('.msg-info-contact-avatar img').attr('src', contactsIdArray[i].img);
+                    // | restore messaggi contatto da array - schermata conversazione
+                    $(contactsIdArray[i].msg).each(
+                        function() {
+                            $('.msg-conversation').append(this).scrollTop($('.msg-conversation').height());
+                        }
+                    );
+                }
+            }
+        // | colore sfondo box contatto
+        $(this).addClass('contact-click');
+        // | allert connessione telefono add hidden
+        $('.stay-connected-phone').addClass('hidden');
+        // | schermata conversazione remove hidden
+        $('.message-box').removeClass('hidden');
         }
     });
-    // CHEVRON SHOW/HIDDEN IN BOX MESSAGGIO **** END
-
-    // NASCONDI NOTIFICA **** START
+    // ***************************
+    // NASCONDI NOTIFICA |||
     $('.enable-notify-box').click(
         function() {
             $(this).slideUp(200);
         }
     );
-    // NASCONDI NOTIFICA **** END
-
     // ***************************
 });
 
 // ***************************
-function sendMessage() {
+function sendMessage(arrayContact) {
     if ($('.msg-input input').val().trim().length !== 0) {
-        var newMessage = $('.template .message').clone();
-        newMessage.find('.message-text').text($('.msg-input input').val());
+        // | creazione data corrente
         var data = new Date();
-        newMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
-        newMessage.addClass('msg-sent');
-        $('.msg-conversation').append(newMessage).scrollTop($('.msg-conversation').height());
+        // *************************
+        var templateMessage = $('.template .message').clone();
+        // | assegnazione testo input al messaggio
+        templateMessage.find('.message-text').text($('.msg-input input').val());
+        // | assegnazione data corrente
+        templateMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
+        // | aggiunta classe
+        templateMessage.addClass('msg-sent');
+        // | push del messaggio nell'array del contatto selezionato
+        for (let i = 0; i < arrayContact.length; i++) {
+            if (arrayContact[i].isSelected) {
+                arrayContact[i].msg.push(templateMessage);
+            }
+        }
+        // | push del messaggio nella schermata conversazione | scrollbar ultimo messaggio
+        $('.msg-conversation').append(templateMessage).scrollTop($('.msg-conversation').height());
+        // | reset input
         $('.msg-input input').val('');
         return true;
     }
+    // | reset input
     $('.msg-input input').val('');
     return false;
 };
 
-function MessageReceived() {
-    var newMessage = $('.template .message').clone();
-    newMessage.find('.message-text').text('Ok');
+function incomingMessage(arrayContact) {
+    // | creazione data corrente
     var data = new Date();
-    newMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
-    newMessage.addClass('msg-received');
-    $('.msg-conversation').append(newMessage).scrollTop($('.msg-conversation').height());
+    // *************************
+    var templateMessage = $('.template .message').clone();
+    // | assegnazione testo input al messaggio
+    templateMessage.find('.message-text').text('ok');
+    // | assegnazione data corrente
+    templateMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
+    // | aggiunta classe
+    templateMessage.addClass('msg-received');
+    // | push del messaggio nell'array del contatto selezionato
+    for (let i = 0; i < arrayContact.length; i++) {
+        if (arrayContact[i].isSelected) {
+            arrayContact[i].msg.push(templateMessage);
+        }
+    }
+    // | push del messaggio nella schermata conversazione | scrollbar ultimo messaggio
+    $('.msg-conversation').append(templateMessage).scrollTop($('.msg-conversation').height());
 };
+
+function messageBoxReset() {
+    $('.msg-conversation .message').each(
+        function() {
+            $(this).remove();
+        }
+    );
+}
 
 function resetIconSend() {
     if ($('.msg-input input').val().length === 0) {
@@ -141,20 +229,9 @@ function resetIconSend() {
     }
 };
 
-function contactIsSelected(thisContact) {
-    if ($(thisContact).css("background-color") == "rgb(235, 235, 235)") {
-        return true;
-    }
-    return false;
-};
-
-function contactFocus(thisContact) {
-    // **********************************************************
-    $('.message-box .msg-info-contact .msg-info-contact-name h4').text($(thisContact).find('.contact-name h4').text());
-    // **********************************************************
-    $('.message-box .msg-info-contact .msg-info-contact-avatar img').attr('src', $(thisContact).find('.contact-avatar img').attr('src'));
-    // **********************************************************
-};
+function contactIdFinder(thisElement) {
+    return $(thisElement).attr('contact-id');
+}
 
 function addZero(number) {
     if (number < 10) {
