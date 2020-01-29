89dce4a24e3536ea19812961438c026ca422b22f
Nicola <nicola.ba1989@gmail.com>
2020-01-29 01:45:11 +0100

    [HTML-CSS-JS] JS riscritto | HTML-CSS leggera ottimizzazione --> da sistemare.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     // ***************************
+    // ********array*based********
+    // ***************************
+    var thisSelectedMessageToDelete;
+    // ***************************
     // ARRAY CONTATTI ****
     var contactsIdArray = [
         { name : 'Bill Gates',
         id : 1000,
         img : 'img/contact-1.jpg',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'Linus Benedict Torvalds',
+        id : 1100,
+        img : 'https://i.imgur.com/hammpug.jpg',
         isSelected : false,
         msg : []
         },
@@ -20,6 +30,36 @@ $(document).ready(function() {
         isSelected : false,
         msg : []
         },
+        { name : 'BoolBOT',
+        id : 1003,
+        img : 'https://i.imgur.com/FCGEGYo.png',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'BoolBOT',
+        id : 1004,
+        img : 'https://i.imgur.com/FCGEGYo.png',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'BoolBOT',
+        id : 1005,
+        img : 'https://i.imgur.com/FCGEGYo.png',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'BoolBOT',
+        id : 1006,
+        img : 'https://i.imgur.com/FCGEGYo.png',
+        isSelected : false,
+        msg : []
+        },
+        { name : 'BoolBOT',
+        id : 1007,
+        img : 'https://i.imgur.com/FCGEGYo.png',
+        isSelected : false,
+        msg : []
+        },
     ];
     // ***************************
     // CREAZIONE BOX CONTATTI da Array |||
@@ -31,7 +71,7 @@ $(document).ready(function() {
         // | nome contatto
         templateContact.find('.contact-name h4').text(contactsIdArray[i].name);
         // | orario ultimo messaggio contatto
-        templateContact.find('.contact-name small').text('00:00');
+        templateContact.find('.contact-name small').text('00:01');
         // | ultimo messaggio contatto
         templateContact.find('.contact-last-message p').text('TEXT');
         // | assegnazione id contatto
@@ -85,18 +125,68 @@ $(document).ready(function() {
                     function() {
                         incomingMessage(contactsIdArray);
                     }, 1000);
+            $('.msg-input input').focus()
             }
         }
     );
     // ***************************
-    // CHEVRON SHOW/HIDDEN IN BOX MESSAGGIO || BOX CONTATTO |||
+    // CHEVRON BOX MESSAGGIO + DROPDOWN |||
+    // | in action - chevron
+    // ***************************
     $(document).on("mouseenter mouseleave", ".message", function(e) {
+        // ***************************
         if (e.type == "mouseenter") {
-            $(this).find('.chevron').removeClass('hidden');
+            if (checkDropdown()) {
+                $(this).find('.chevron-msg').removeClass('hidden');
+            }
         } else {
-            $(this).find('.chevron').addClass('hidden');
+            if (checkDropdown()) {
+                $(this).find('.chevron-msg').addClass('hidden');
+            }
         }
     });
+    // ***************************
+    // | in action - dropdown
+    // ***************************
+    $(document).on('click', '.chevron-msg', function(event) {
+        thisSelectedMessageToDelete = $(this).parents('.message');
+        // | blocco propagazione
+        event.stopPropagation();
+        $('.dropdown-message-wrapper').css({
+            "left": event.pageX + 'px',
+            "top": event.pageY + 'px'
+            }
+        );
+        $('.dropdown-message-wrapper').removeClass('hidden');
+    });
+    // ***************************
+    $(document).on('click', function() {
+        if (!checkDropdown()) {
+            $('.dropdown-message-wrapper').addClass('hidden');
+            $('.chevron-msg').addClass('hidden');
+            thisSelectedMessageToDelete;
+        }
+    });
+    // ***************************
+    $(document).on('click', '#delete-msg', function(event) {
+        for (let i = 0; i < contactsIdArray.length; i++) {
+            if (contactsIdArray[i].isSelected) {
+                var idContactToDelete = contactsIdArray[i].id;
+                $(contactsIdArray[i].msg).each(
+                    function() {
+                        if (this.id == $(thisSelectedMessageToDelete).attr('id')) {
+                            this.type = 'deleted';
+                            thisSelectedMessageToDelete.remove();
+                            thisSelectedMessageToDelete;
+                            pairingMessageContactBox(idContactToDelete, contactsIdArray)
+                        }
+                    }
+                );
+            }
+        }
+    });
+    // ***************************
+    // BOX CONTATTI |||
     // ***************************
     $(document).on("mouseenter mouseleave click", ".contact", function(e) {
         if (e.type == "mouseenter") {
@@ -139,7 +229,24 @@ $(document).ready(function() {
                     // | restore messaggi contatto da array - schermata conversazione
                     $(contactsIdArray[i].msg).each(
                         function() {
-                            $('.msg-conversation').append(this).scrollTop($('.msg-conversation').height());
+                            if (this.type != 'deleted') {
+                                // | message builder
+                                var templateMessage = $('.template .message').clone();
+                                // | assegnazione testo input al messaggio
+                                templateMessage.find('.message-text').text(this.text);
+                                // | assegnazione data corrente
+                                templateMessage.find('.message-time').text(this.h);
+                                // | aggiunta classe
+                                if (this.type == 'sent') {
+                                    templateMessage.addClass('msg-sent');
+                                } else {
+                                    templateMessage.addClass('msg-received');
+                                }
+                                templateMessage.addClass('msg-sent');
+                                // | assegnazione id messaggio
+                                templateMessage.attr('id', this.id);
+                                $('.msg-conversation').append(templateMessage).scrollTop($('.msg-conversation').height());
+                            }
                         }
                     );
                 }
@@ -161,9 +268,9 @@ $(document).ready(function() {
     );
     // ***************************
 });
-
 // ***************************
 // **********function*********
+// ***************************
 // ***************************
 function sendMessage(arrayContact) {
     if ($('.msg-input input').val().trim().length !== 0) {
@@ -172,16 +279,29 @@ function sendMessage(arrayContact) {
         // *************************
         var templateMessage = $('.template .message').clone();
         // | assegnazione testo input al messaggio
-        templateMessage.find('.message-text').text($('.msg-input input').val());
+        let inputVal = $('.msg-input input').val();
+        if (inputVal == '/lorem') {
+            inputVal = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        }
+        templateMessage.find('.message-text').text(inputVal);
         // | assegnazione data corrente
-        templateMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
+        let rty = addZero(data.getHours()) + ':' + addZero(data.getMinutes());
+        templateMessage.find('.message-time').text(rty);
         // | aggiunta classe
         templateMessage.addClass('msg-sent');
+        // | assegnazione id messaggio
+        let xyz =  Date.now();
+        templateMessage.attr('id', xyz);
         // | push del messaggio nell'array del contatto selezionato + pairingMessageContactBox
         for (let i = 0; i < arrayContact.length; i++) {
             if (arrayContact[i].isSelected) {
-                arrayContact[i].msg.push(templateMessage);
-                pairingMessageContactBox(arrayContact[i].id, templateMessage);
+                arrayContact[i].msg.push({
+                    id : xyz,
+                    type : 'sent',
+                    h : rty,
+                    text : inputVal
+                });
+                pairingMessageContactBox(arrayContact[i].id, arrayContact);
             }
         }
         // | push del messaggio nella schermata conversazione | scrollbar ultimo messaggio
@@ -196,38 +316,68 @@ function sendMessage(arrayContact) {
 };
 
 function incomingMessage(arrayContact) {
+    var resp = 'ok'
     // | creazione data corrente
     var data = new Date();
     // *************************
     var templateMessage = $('.template .message').clone();
     // | assegnazione testo input al messaggio [preimpostato]
-    templateMessage.find('.message-text').text('ok');
+    templateMessage.find('.message-text').text(resp);
     // | assegnazione data corrente
-    templateMessage.find('.message-time').text(addZero(data.getHours()) + ':' + addZero(data.getMinutes()));
+    let rty = addZero(data.getHours()) + ':' + addZero(data.getMinutes());
+    templateMessage.find('.message-time').text(rty);
     // | aggiunta classe
     templateMessage.addClass('msg-received');
+    // | assegnazione id messaggio
+    let xyz =  Date.now();
+    templateMessage.attr('id', xyz);
     // | push del messaggio nell'array del contatto selezionato + pairingMessageContactBox
     for (let i = 0; i < arrayContact.length; i++) {
         if (arrayContact[i].isSelected) {
-            arrayContact[i].msg.push(templateMessage);
-            pairingMessageContactBox(arrayContact[i].id, templateMessage);
+            arrayContact[i].msg.push({
+                id : xyz,
+                type : 'received',
+                h : rty,
+                text : resp
+            });
+            pairingMessageContactBox(arrayContact[i].id, arrayContact);
         }
     }
     // | push del messaggio nella schermata conversazione | scrollbar ultimo messaggio
     $('.msg-conversation').append(templateMessage).scrollTop($('.msg-conversation').height());
 };
 
-function pairingMessageContactBox(idFromArray, tmplMessage) {
+function pairingMessageContactBox(idFromArray, arrayContact) {
+    var lastMsg;
+    for (let i = 0; i < arrayContact.length; i++) {
+        if (arrayContact[i].id == idFromArray) {
+            lastMsg = arrayContact[i].msg[arrayContact[i].msg.length - 1];
+        }
+    }
     $('.contacts-list .contact').each(
         function() {
             if (contactIdFinder(this) == idFromArray) {
-                // | orario ultimo messaggio contatto
-                $(this).find('.contact-name small').text(tmplMessage.find('.message-time').text());
-                // | ultimo messaggio contatto
-                $(this).find('.contact-last-message p').text(tmplMessage.find('.message-text').text());
+                if (lastMsg.type == 'deleted') {
+                    // | orario ultimo messaggio contatto
+                    $(this).find('.contact-name small').text(lastMsg.h);
+                    // | messaggio contatto contatto eliminato + italic
+                    $(this).find('.contact-last-message p').text('Messaggio eliminato').css('font-style', 'italic');
+                } else {
+                    // | orario ultimo messaggio contatto
+                    $(this).find('.contact-name small').text(lastMsg.h);
+                    // | ultimo messaggio contatto + normal
+                    $(this).find('.contact-last-message p').text(lastMsg.text).css('font-style', 'normal');
+                }
             }
         }
     );
+}
+
+function checkDropdown() {
+    if ($('.dropdown-message-wrapper').hasClass('hidden') == false) {
+        return false;
+        }
+    return true;
 }
 
 function messageBoxReset() {
@@ -257,10 +407,4 @@ function addZero(number) {
         number = '0' + number;
     }
     return number;
-};
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
